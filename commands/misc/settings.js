e54fe764da4a6a8dbe8f8800a310591cc2e06d7e
TJDoesCode <tjdoescode@gmail.com>
2017-07-08 16:04:18 -0500

    update for gann toggle

--- commands/misc/settings.js
+++ commands/misc/settings.js
@@ -5,7 +5,6 @@ const oneLine = require('common-tags').oneLine;
 module.exports = class SettingsCommand extends commando.Command {
   constructor(client) {
     super(client, {
-      argsPromptLimit: 0,
       name: 'settings',
       aliases: ['set', 'setting'],
       group: 'misc',
@@ -14,8 +13,9 @@ module.exports = class SettingsCommand extends commando.Command {
       details: oneLine `
 				This command allows you to set server settings.
         This is required for many comamnds to work.
+        Permission is locked to users with the server administrator permission.
 			`,
-      examples: ['settings add starboard #starboard'],
+      examples: ['settings add mod @Moderators'],
 
       args: [{
           key: 'action',
@@ -37,37 +37,71 @@ module.exports = class SettingsCommand extends commando.Command {
           type: 'string',
           prompt: '',
           default: '',
-          infinite: true
+          infinite: false
         }
       ],
-
       guildOnly: true,
-
       guarded: true
     });
   }
 
+  //eslint-disable-next-line class-methods-use-this
   hasPermission(msg) {
-    if (!msg.guild) return this.client.isOwner(msg.author);
-
     return msg.member.hasPermission('ADMINISTRATOR');
   }
 
   //eslint-disable-next-line class-methods-use-this
   async run(message, args) {
     if (args.action.toLowerCase() === 'add') {
-      if (args.setting.toLowerCase() === 'starboard') {
+      if (args.setting.toLowerCase() === 'mod') {
+        const rawRole = message.mentions.roles.first()
+        if (!rawRole) return message.reply('Please specify a role to set as the mod role!')
+        const roleToLog = rawRole.id
+        message.guild.settings.set('modrole', roleToLog)
+        message.reply(`Set the mod role to "<@&${message.guild.settings.get('modrole')}>"`)
+      } else if (args.setting.toLowerCase() === 'admin') {
+        const rawRole = message.mentions.roles.first()
+        if (!rawRole) return message.reply('Please specify a role to set as the admin role!')
+        const roleToLog = rawRole.id
+        message.guild.settings.set('adminrole', roleToLog)
+        message.reply(`Set the admin role to "<@&${message.guild.settings.get('adminrole')}>"`)
+      } else if (args.setting.toLowerCase() === 'modlog') {
         const rawChan = message.mentions.channels.first()
-        if (!rawChan) return message.reply('Please specify a channel to set as the starboard!')
+        if (!rawChan) return message.reply('Please specify a channel to use for the mod logs!')
         const chanToLog = rawChan.id
-        message.guild.settings.set('starboard', chanToLog)
-        message.reply(`Set the starboard to "<#${message.guild.settings.get('starboard')}>"`)
+        message.guild.settings.set('modlog', chanToLog)
+        message.reply(`Set the mod log channel to "<#${message.guild.settings.get('modlog')}>"`)
+      } else if (args.setting.toLowerCase() === 'announcements') {
+        const state = args.value
+        if (state.toLowerCase() !== 'on') {
+          //eslint-disable-next-line no-useless-escape
+          if (state.toLowerCase() !== 'off') return message.reply('Invaid state! Use \`on\` or  \`off\`.')
+        }
+        message.guild.settings.set('announcements', state)
+        message.reply(`Set the announcement state to "${message.guild.settings.get('announcements')}" \nDo \`${message.guild.commandPrefix}settings add announcements on\` to re-enable announcements.`)
+      } else if (args.setting.toLowerCase() === 'autorole') {
+        const rawRole = message.mentions.roles.first()
+        if (!rawRole) return message.reply('Please specify a role to set as the auto role!')
+        const roleToLog = rawRole.id
+        message.guild.settings.set('autorole', roleToLog)
+        message.reply(`Set the autorole to "<@&${message.guild.settings.get('autorole')}>"`)
       } else {
         message.reply('That\'s not a setting. Please try again.');
       }
-      //eslint-disable-next-line no-empty
     } else if (args.action.toLowerCase() === 'view') {
-
+      if (args.setting.toLowerCase() === 'mod') {
+        message.reply(`The mod role is "<@&${message.guild.settings.get('modrole')}>"`)
+      } else if (args.setting.toLowerCase() === 'admin') {
+        message.reply(`The admin role is "<@&${message.guild.settings.get('adminrole')}>"`)
+      } else if (args.setting.toLowerCase() === 'modlog') {
+        message.reply(`The mod log channel is "<#${message.guild.settings.get('modlog')}>"`)
+      } else if (args.setting.toLowerCase() === 'announcements') {
+        message.reply(`The announcements state is "${message.guild.settings.get('announcements')}"`)
+      } else if (args.setting.toLowerCase() === 'autorole') {
+        message.reply(`The autorole is "<&${message.guild.settings.get('autorole')}"`)
+      } else {
+        message.reply('That\'s not a setting. Please try again.');
+      }
     } else {
       message.reply('Invalid command usage. Please try again.');
     }
