8d9a400c87984719469bf398f62f6182d3d8d59c
TJDoesCode <tjdoescode@gmail.com>
2017-07-10 19:36:05 -0500

    add bot guild auto-leave and guild activity logging

--- dsmores.js
+++ dsmores.js
@@ -88,18 +88,44 @@ client
     console.log(`New guild added:
 Guild: ${guild.id}
 Name: ${guild.name}
-Owner: ${guild.owner.tag} (${guild.owner.id})
+Owner: ${guild.owner.user.tag} (${guild.owner.id})
 Members: ${guild.members.size}
+Bots: ${guild.members.filter(u => u.user.bot).size} (${Math.floor(guild.members.filter(u => u.user.bot).size / guild.members.size * 100)}%)
+Humans: ${guild.members.filter(u => !u.user.bot).size} (${Math.floor(guild.members.filter(u => !u.user.bot).size / guild.members.size * 100)}%)
 Now on: ${client.guilds.size} servers`)
+    client.channels.get('330701184698679307').send(`New guild added:
+Guild: ${guild.id}
+Name: ${guild.name}
+Owner: ${guild.owner.user.tag} (${guild.owner.id})
+Members: ${guild.members.size}
+Bots: ${guild.members.filter(u => u.user.bot).size} (${Math.floor(guild.members.filter(u => u.user.bot).size / guild.members.size * 100)}%)
+Humans: ${guild.members.filter(u => !u.user.bot).size} (${Math.floor(guild.members.filter(u => !u.user.bot).size / guild.members.size * 100)}%)
+Now on: ${client.guilds.size} servers`)
+    let botPercentage = Math.floor(guild.members.filter(u => u.user.bot).size / guild.members.size * 100)
+    if (botPercentage >= 80) {
+      guild.defaultChannel.send('**ALERT:** Your guild has been marked as an illegal guild. \nThis may be due to it being marked as a bot guild or marked as a spam guild. \nThe bot will now leave this server. \nIf you wish to speak to my developer, you may join here: https://discord.gg/t8xHbHY')
+      guild.owner.send('**ALERT:** Your guild has been marked as an illegal guild. \nThis may be due to it being marked as a bot guild or marked as a spam guild. \nThe bot will now leave this server. \nIf you wish to speak to my developer, you may join here: https://discord.gg/t8xHbHY')
+      guild.leave()
+    }
     client.user.setGame(`s.help | ${client.guilds.size} servers`)
-    guild.settings.set('announcements', 'on')
+    if (guild) guild.settings.set('announcements', 'on')
   })
   .on('guildDelete', (guild) => {
     console.log(`Exsisting guild left:
 Guild: ${guild.id}
 Name: ${guild.name}
-Owner: ${guild.owner.tag} (${guild.owner.id})
+Owner: ${guild.owner.user.tag} (${guild.owner.id})
 Members: ${guild.members.size}
+Bots: ${guild.members.filter(u => u.user.bot).size} (${Math.floor(guild.members.filter(u => u.user.bot).size / guild.members.size * 100)}%)
+Humans: ${guild.members.filter(u => !u.user.bot).size} (${Math.floor(guild.members.filter(u => !u.user.bot).size / guild.members.size * 100)}%)
+Now on: ${client.guilds.size} servers`)
+    client.channels.get('330701184698679307').send(`Exsisting guild left:
+Guild: ${guild.id}
+Name: ${guild.name}
+Owner: ${guild.owner.user.tag} (${guild.owner.id})
+Members: ${guild.members.size}
+Bots: ${guild.members.filter(u => u.user.bot).size} (${Math.floor(guild.members.filter(u => u.user.bot).size / guild.members.size * 100)}%)
+Humans: ${guild.members.filter(u => !u.user.bot).size} (${Math.floor(guild.members.filter(u => !u.user.bot).size / guild.members.size * 100)}%)
 Now on: ${client.guilds.size} servers`)
     client.user.setGame(`s.help | ${client.guilds.size} servers`)
   })
