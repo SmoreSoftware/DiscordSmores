8d530522d9d3b9bc811f09afb22f370fd233046f
Chronomly6 <Chronomly6@users.noreply.github.com>
2017-07-12 16:22:53 -0500

    patch guild count

--- dsmores.js
+++ dsmores.js
@@ -107,7 +107,7 @@ Now on: ${client.guilds.size} servers`)
       guild.owner.send(`**ALERT:** Your guild, "${guild.name}", has been marked as an illegal guild. \nThis may be due to it being marked as a bot guild or marked as a spam guild. \nThe bot will now leave the server. \nIf you wish to speak to my developer, you may join here: https://discord.gg/t8xHbHY`)
       guild.leave()
     }
-    client.user.setGame(`s.help | ${client.guilds.size} servers`)
+    client.user.setGame(`ds.help | ${client.guilds.size} servers`)
     if (guild) guild.settings.set('announcements', 'on')
   })
   .on('guildDelete', (guild) => {
@@ -127,7 +127,7 @@ Members: ${guild.members.size}
 Bots: ${guild.members.filter(u => u.user.bot).size} (${Math.floor(guild.members.filter(u => u.user.bot).size / guild.members.size * 100)}%)
 Humans: ${guild.members.filter(u => !u.user.bot).size} (${Math.floor(guild.members.filter(u => !u.user.bot).size / guild.members.size * 100)}%)
 Now on: ${client.guilds.size} servers`)
-    client.user.setGame(`s.help | ${client.guilds.size} servers`)
+    client.user.setGame(`ds.help | ${client.guilds.size} servers`)
   })
 
 client.login(config.token).catch(console.error);
